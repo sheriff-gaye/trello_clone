b2288a0d4e46137a387531b69ff5b362734ccfff
sheriff-gaye <sheriffgaye5@gmail.com>
2024-10-22 22:52:13 +0000

    all

--- app/(marketing)/_components/NavBar.tsx
+++ app/(marketing)/_components/NavBar.tsx
@@ -4,14 +4,14 @@ import { Logo } from '../../../components/logo';
 
 const NavBar = () => {
   return (
-    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+    <div className="fixed top-0 w-full h-20 px-4 border-b shadow-sm bg-white flex items-center">
       <div className="md:max-w-screen-2xl flex  items-center w-full  justify-between mx-auto">
         <Logo />
         <div className="space-x-4  md:block md:w-auto flex  items-center justify-between w-full">
-          <Button variant="outline" size="sm" asChild>
+          <Button size="sm" asChild>
             <Link href="/sign-in">Login</Link>
           </Button>
-          <Button size="sm" asChild>
+          <Button size="sm" asChild variant="primary">
             <Link href="/sign-up">Get Taskify for free</Link>
           </Button>
         </div>
